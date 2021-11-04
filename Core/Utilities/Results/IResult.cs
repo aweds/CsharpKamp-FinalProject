e0068604f3e3033035420f2e0ded2834a805fe13
Osman Kırcı <osman_kirci@windowslive.com>
2021-11-04 23:57:26 +0300

    10. gün eğitim sonu

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    // temel voidler için başlangıc kodu.   
+    public interface IResult
+    {
+        // işlemin başarılı olup olmadıgını dondurur
+        bool Success { get; }
+        // kullanıcıya yapılan işlem ile alakalı mesaj vericek metot.
+        string Message { get; }
+    }
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base( true,message)
+        {
+            // mesaj verilerek kullanılarak başarılı sonuca mesaj oluşturan yapıcı sınıf
+        }
+        public SuccessResult() :base(true)
+        {
+            // mesaj vermeden başarılı sonuç için kullanacagımız yapıcı sınıf
+        }
+    }
+}
