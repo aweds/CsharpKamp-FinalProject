6b284bcdf446cd0e524bbbdc4d9f7da224513020
Osman Kırcı <osman_kirci@windowslive.com>
2021-11-04 12:15:34 +0300

    4 kasım 2. mola

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categorDal;
+
+        public CategoryManager(ICategoryDal categorDal)
+        {
+            _categorDal = categorDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categorDal.GetAll();
+        }
+
+        // select * from Categories where categoryId = id
+        public Category GetById(int id)
+        {
+            return _categorDal.Get(p => p.CategoryId == id);
+        }
+    }
+}
