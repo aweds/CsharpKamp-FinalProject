3c5a468b2013d5e00143432671c33d473bd6be48
Osman Kırcı <osman_kirci@windowslive.com>
2021-11-07 15:34:20 +0300

    11 ders güncellemeleri

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -21,7 +21,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 23)
+            if (DateTime.Now.Hour == 22)// saat 22 de sistem bakıma giriyor simulasyonu
             { 
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
@@ -42,12 +42,12 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            if (DateTime.Now.Hour == 23)
+            if (DateTime.Now.Hour == 22)// saat 22 de sistem bakıma giriyor simulasyonu
             {
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             }
 
-            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails(),Messages.ProductsListed);
         }
         
         // verilen id'ye gore bir urun geri dondurur
---
+++ WebApi2/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled  gevşej bir bağlılık var aralrında
+        // naming convention
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //[HttpGet]
+        //public List<Product> Get()
+        //{
+        //    _productService = new ProductManager(new EfProductDal());
+        //    var result = _productService.GetAll();
+        //    return result.Data;
+
+        //    //return "Merhaba";
+        //}
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            _productService = new ProductManager(new EfProductDal());
+            var result = _productService.GetAll();
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(Product product)
+        { 
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
